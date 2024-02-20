0c9335ac28943cded305eada836a961f71278d38
skaram82 <stelios.karamanidis@gmail.com>
2024-02-20 13:06:41 +0200

    Initial commit

---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/jaydanurwin/',
+  },
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://twitter.com/littlesticksdev',
+  },
+  {
+    title: 'TikTok',
+    icon: 'ic:baseline-tiktok',
+    url: 'https://youtube.com/c/LittleSticks',
+  },
+]
