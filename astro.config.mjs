c4e9be74e5cef6e6c167709d191595a0ad542cb5
skaram82 <stelios.karamanidis@gmail.com>
2024-02-28 01:12:40 +0200

    Change landing page content and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
